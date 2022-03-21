bad8de40ccc1b162afb714e8784e01ea02ee1349
aosothra <arhdam@yandex.ru>
2022-03-21 20:06:27 +0400

    Visit class enchanced with bussiness logic

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import localtime
 
 
 class Passcard(models.Model):
@@ -19,6 +20,25 @@ class Visit(models.Model):
     entered_at = models.DateTimeField()
     leaved_at = models.DateTimeField(null=True)
 
+    def is_strange(self, standard_duration=60):
+        """Checks wether visit duration is compliant with the standard duration.
+
+        Normal security protocols define standard duration of 60 minutes.
+        """
+
+        visit_duration = self.get_duration()
+        return visit_duration > standard_duration*60
+
+    def get_duration(self) -> float:
+        """Returns time duration of the visit in seconds"""
+
+        time_last = (
+            self.leaved_at
+            if self.leaved_at else localtime()
+        )
+        duration = time_last - self.entered_at
+        return duration.total_seconds()
+
     def __str__(self):
         return '{user} entered at {entered} {leaved}'.format(
             user=self.passcard.owner_name,
@@ -28,3 +48,11 @@ class Visit(models.Model):
                 if self.leaved_at else 'not leaved'
             )
         )
+    
+    @staticmethod
+    def format_duration(seconds):
+        """Converts time duration in seconds to a readable format"""
+        
+        hours = int(seconds // 3600)
+        minutes = int((seconds % 3600) // 60)
+        return f'{hours}h {minutes}m'
